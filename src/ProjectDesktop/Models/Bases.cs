93e88880453537963dc004c31a4e6c51d3cf4558
Thelmarciel <=>
2025-06-03 19:46:56 -0300

    Adicionando as library no projeto

---
+++ src/ProjectDesktop/Models/Bases.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectDesktop.Models
+{
+    public class Bases
+    {
+        public void DiseñoDatagridview(ref DataGridView Listado)
+        {
+            Listado.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            Listado.EnableHeadersVisualStyles = false;
+            DataGridViewCellStyle cabecera = new DataGridViewCellStyle();
+            cabecera.BackColor = Color.White;
+            cabecera.ForeColor = Color.Black;
+            cabecera.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            Listado.ColumnHeadersDefaultCellStyle = cabecera;
+        }
+        public void DiseñoDatagridviewEliminar(ref DataGridView Listado)
+        {
+            foreach (DataGridViewRow row in Listado.Rows)
+            {
+                string estado;
+                estado = row.Cells["Estado"].Value.ToString();
+                if (estado == "ELIMINADO")
+                {
+                    row.DefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Strikeout | FontStyle.Bold);
+                    row.DefaultCellStyle.ForeColor = Color.Red;
+                }
+            }
+        }
+        public void DiseñoDatagridviewOscuro(ref DataGridView Listado)
+        {
+            Listado.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            Listado.EnableHeadersVisualStyles = false;
+            DataGridViewCellStyle cabecera = new DataGridViewCellStyle();
+            cabecera.BackColor = Color.FromArgb(49, 49, 49);
+            cabecera.ForeColor = Color.White;
+            cabecera.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            Listado.ColumnHeadersDefaultCellStyle = cabecera;
+        }
+        public static TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+        public static MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+        public static string Encriptar(string texto)
+        {
+            string tempEncriptar = null;
+            if (string.IsNullOrEmpty(texto.Trim(' ')))
+            {
+                tempEncriptar = "";
+            }
+            else
+            {
+                des.Key = hashmd5.ComputeHash((new UnicodeEncoding()).GetBytes(Desencryptacion.appPwdUnique));
+                des.Mode = CipherMode.ECB;
+                ICryptoTransform encrypt = des.CreateEncryptor();
+                byte[] buff = UnicodeEncoding.ASCII.GetBytes(texto);
+                tempEncriptar = Convert.ToBase64String(encrypt.TransformFinalBlock(buff, 0, buff.Length));
+            }
+            return tempEncriptar;
+        }
+        public static string Desencriptar(string texto)
+        {
+            string tempDesencriptar = null;
+            if (string.IsNullOrEmpty(texto.Trim(' ')))
+            {
+                tempDesencriptar = "";
+            }
+            else
+            {
+                des.Key = hashmd5.ComputeHash((new UnicodeEncoding()).GetBytes(Desencryptacion.appPwdUnique));
+                des.Mode = CipherMode.ECB;
+                ICryptoTransform desencrypta = des.CreateDecryptor();
+                byte[] buff = Convert.FromBase64String(texto);
+                tempDesencriptar = UnicodeEncoding.ASCII.GetString(desencrypta.TransformFinalBlock(buff, 0, buff.Length));
+            }
+            return tempDesencriptar;
+        }
+        public static void Obtener_serialPC(ref string serial)
+        {
+            ManagementObject serialPC = new ManagementObject("Win32_PhysicalMedia='\\\\.\\PHYSICALDRIVE0'");
+            serial = serialPC.Properties["SerialNumber"].Value.ToString();
+            serial = Encriptar(serial.Trim());
+
+        }
+        public static object Separador_de_Numeros(TextBox CajaTexto, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == '.') || (e.KeyChar == ','))
+            {
+
+                e.KeyChar = System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator[0];
+
+            }
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar == '.' && (~CajaTexto.Text.IndexOf(".")) != 0)
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == '.')
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar == ',')
+            {
+                e.Handled = false;
+
+            }
+            else
+            {
+                e.Handled = true;
+
+            }
+            return null;
+        }
+        public static string ObtenerIp(ref string valorIp)
+        {
+            valorIp = Dns.GetHostEntry(System.Environment.MachineName).AddressList.FirstOrDefault((i) => i.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            return valorIp;
+        }
+    }
+}
---
+++ src/ProjectDesktop/Models/Desencryptacion.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+namespace ProjectDesktop.Models
+{
+    public class Desencryptacion
+    {
+        static private Encryptacion aes = new Encryptacion();
+        static public string CnString;
+        static string dbcnString;
+        static public string appPwdUnique = "wmssistema.olamundo";
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes[0].Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+        internal class label { }
+        public static object UsuariosEncryp()
+        {
+            XmlDocument doc = new XmlDocument();
+            label root = new label();
+            dbcnString = root.ToString();
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+    }
+}
